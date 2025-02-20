4c09892fa7e46b9ddfb3bceefd1d62a569fe4073
Eslam Rasmy <eslamrasme@gmai.com>
2025-02-20 14:21:14 +0200

    .

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -54,7 +54,7 @@ const Nav = () => {
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className=" w-full md:block md:w-auto" id="navbar-default">
             {token ? (
               <>
                 <ul className="font-medium flex flex-col p-4 md:p-1  mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-};
-
-/* //import { defineConfig } from 'vite'
+//import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(), require('flowbite/plugin')],
+  plugins: [react(),tailwindcss()],
 })
- */
